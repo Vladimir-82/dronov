56981af038c758b2a6252f41de44ca80733f2c44
vladimir <grun_gespenst@tut.by>
2021-12-30 12:34:06 +0300

    html ready

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Bd
+from .models import Bd, Rubric
 
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'price', 'photo', 'published')
+    list_display = ('title', 'content', 'price', 'photo', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
 
 admin.site.register(Bd)
+admin.site.register(Rubric)
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -6,6 +6,7 @@ class Bd(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', null=True, blank=True)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
     def __str__(self):
         return self.title
@@ -14,3 +15,15 @@ class Bd(models.Model):
         verbose_name = 'Объявление'
         verbose_name_plural = 'Объявления'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+        ordering = ['name']
